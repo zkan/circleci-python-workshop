f97d631c79c303aea2bdf29817a2c11438c757fb
zkan <kan@prontomarketing.com>
2018-02-06 15:27:33 +0700

    Add more test case (input 6)

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -2,13 +2,17 @@ import unittest
 
 
 def fizzbuzz(number):
-    if number == 3:
+    if number % 3 == 0:
         return 'fizz'
 
 
 class FizzBuzzTest(unittest.TestCase):
     def test_input_3_should_get_fizz(self):
         result = fizzbuzz(3)
+        self.assertEqual(result, 'fizz')
+
+    def test_input_6_should_get_fizz(self):
+        result = fizzbuzz(6)
         self.assertEqual(result, 'fizz')
 
 
